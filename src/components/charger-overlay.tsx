177d3a5aec0319e1371d0f8a1a506f9ec0b78dca
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-06-16 15:26:34 +0200

    feat: copy address button

--- src/components/charger-overlay.tsx
+++ src/components/charger-overlay.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+import { TbCopy, TbCopyCheck } from 'react-icons/tb';
+
 import { ChargePointPlug } from '@/components/charge-point-plug';
 import { useChargePointDetails } from '@/hooks/use-charge-point-details';
 import { MobilitiData } from '@/types/mobiliti.types';
@@ -7,11 +10,25 @@ interface ChargerOverlayProps {
 }
 
 export function ChargerOverlay({ data }: ChargerOverlayProps) {
+  const [copied, setCopied] = useState(false);
   const chargePointDetails = useChargePointDetails(
     data.ocpi?.stationId?.countryCode ?? 'HU',
     data.ocpi?.stationId?.partyId ?? '',
     data.ocpi?.stationId?.locationId ?? ''
   );
+
+  const onAddressCopy = () => {
+    if (!navigator.clipboard || !data.address || !data.city) return;
+    navigator.clipboard.writeText(`${data.address}, ${data.city}`);
+    setCopied(true);
+  };
+
+  useEffect(() => {
+    if (copied) {
+      const to = setTimeout(() => setCopied(false), 2000);
+      return () => clearTimeout(to);
+    }
+  }, [copied]);
 
   return (
     <div className='bg-white shadow-md rounded-md p-2 w-60 space-y-2'>
@@ -25,6 +42,15 @@ export function ChargerOverlay({ data }: ChargerOverlayProps) {
         />
       ))}
       <p className='italic text-slate-500'>{data.operator?.name ?? data.ocpi?.stationId?.partyId}</p>
+      {data.address && data.city && navigator.clipboard && (
+        <button
+          onClick={onAddressCopy}
+          className='bg-blue-500 p-2 rounded-md flex space-x-1 text-white w-fit items-center'
+        >
+          {copied ? <TbCopyCheck size={20} /> : <TbCopy size={20} />}
+          {copied ? 'Másolva' : 'Cím másolása'}
+        </button>
+      )}
     </div>
   );
 }
