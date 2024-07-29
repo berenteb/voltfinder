ff446dd96db5fc00ba695d9d94bfbd290a90ba5f
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-07-29 16:55:55 +0200

    fix: disable cache

--- src/app/api/dcs/charge-points/route.ts
+++ src/app/api/dcs/charge-points/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 import { getPoolDetails, getPoolList } from '@/services/server/dcs.service';
 
+export const dynamic = 'force-dynamic';
+
 export const GET = async () => {
   const pools = await getPoolList();
   const poolDetails = await getPoolDetails(pools.map((pool) => pool.id));
