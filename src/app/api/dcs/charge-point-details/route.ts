017dd3d3b0482314e5634453b2d9f8fb5a34c284
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-08-11 13:38:20 +0200

    fix: charge point details cache to off

--- src/app/api/dcs/charge-point-details/route.ts
+++ src/app/api/dcs/charge-point-details/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 import { getChargePointsByIds, getPoolList } from '@/services/server/dcs.service';
 
+export const dynamic = 'force-dynamic';
+
 export const GET = async () => {
   const pools = await getPoolList();
   const pointsIds = pools.reduce<string[]>((acc, pool) => {
