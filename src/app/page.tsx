6a7ea5b2729c67bc17278455eeebe617c0097d5b
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-08-15 21:11:02 +0200

    fix: add missing suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Suspense } from 'react';
 
 import { MapComponent } from '@/app/map';
 import { FirebaseProvider } from '@/components/firebase-context';
@@ -12,14 +13,16 @@ const queryClient = new QueryClient();
 export default function Home() {
   return (
     <main className='h-full w-full'>
-      <QueryClientProvider client={queryClient}>
-        <FirebaseProvider>
-          <LocationProvider>
-            <MapComponent />
-          </LocationProvider>
-        </FirebaseProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <QueryClientProvider client={queryClient}>
+          <FirebaseProvider>
+            <LocationProvider>
+              <MapComponent />
+            </LocationProvider>
+          </FirebaseProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </Suspense>
     </main>
   );
 }
