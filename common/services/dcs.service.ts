5cef1936b1d4f227ab97ef58564e75d52e25db83
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-09-05 19:19:05 +0200

    fix: moved bounds object

--- common/services/dcs.service.ts
+++ common/services/dcs.service.ts
@@ -9,7 +9,13 @@ import {
 } from '@/common/types/dcs.types';
 import { DcsPoolDetails, DcsPoolDetailsRequestDto, DcsPoolDetailsResponseDto } from '@/common/types/dcs-pool-details';
 import { ConnectorPriceRequestDto, DcsPriceRequestDto, DcsPriceResponseDto } from '@/common/types/dcs-price.types';
-import { MapBounds } from '@/config/bound';
+
+const MapBounds = {
+  latitudeNW: 48.6238540716,
+  longitudeNW: 16.2022982113,
+  latitudeSE: 45.7594811061,
+  longitudeSE: 22.710531447,
+};
 
 type DcsRestApiPath = 'clusters' | 'charge-points' | 'pools';
 
--- src/config/bound.ts
+++
@@ -1,6 +0,0 @@
-export const MapBounds = {
-  latitudeNW: 48.6238540716,
-  longitudeNW: 16.2022982113,
-  latitudeSE: 45.7594811061,
-  longitudeSE: 22.710531447,
-};
